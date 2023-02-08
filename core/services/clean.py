7c33d4f3f59b78401095b8a72952522941dc8276
Mohammad Iskandarani <M7md.k.isk@gmail.com>
2023-02-08 13:46:38 +0300

    Remove category encoding

--- core/services/clean.py
+++ core/services/clean.py
@@ -1,19 +1,31 @@
-
 import os
 import sys
 from timeit import default_timer as timer
 import pandas as pd
 from .modules import *
 
-class AutoClean:
 
-    def __init__(self, input_data, mode='auto', duplicates=False, missing_num=False, missing_categ=False, encode_categ=False, extract_datetime=False, outliers=False, outlier_param=1.5, logfile=True, verbose=False):  
-        '''
+class AutoClean:
+    def __init__(
+        self,
+        input_data,
+        mode="auto",
+        duplicates=False,
+        missing_num=False,
+        missing_categ=False,
+        encode_categ=False,
+        extract_datetime=False,
+        outliers=False,
+        outlier_param=1.5,
+        logfile=True,
+        verbose=False,
+    ):
+        """
         input_data (dataframe)..........Pandas dataframe
         mode (str)......................define in which mode you want to run AutoClean
                                         'auto' = sets all parameters to 'auto' and let AutoClean do the data cleaning automatically
                                         'manual' = lets you choose which parameters/cleaning steps you want to perform
-                                        
+
         duplicates (str)................define if duplicates in the data should be handled
                                         duplicates are rows where all features are identical
                                         'auto' = automated handling, deletes all copies of duplicates except one
@@ -51,15 +63,22 @@ class AutoClean:
         logfile (bool)..................define whether to create a logile during the AutoClean process
                                         logfile will be saved in working directory as "autoclean.log"
         verbose (bool)..................define whether AutoClean logs will be printed in console
-        
+
         OUTPUT (dataframe)..............a cleaned Pandas dataframe, accessible through the 'output' instance
-        '''
+        """
         start = timer()
-        
+
         output_data = input_data.copy()
 
-        if mode == 'auto':
-            duplicates, missing_num, missing_categ, outliers, encode_categ, extract_datetime = 'auto', 'auto', 'auto', 'winz', ['auto'], 's'
+        if mode == "auto":
+            (
+                duplicates,
+                missing_num,
+                missing_categ,
+                outliers,
+                encode_categ,
+                extract_datetime,
+            ) = ("auto", "auto", "auto", "winz", ["auto"], "s")
 
         self.mode = mode
         self.duplicates = duplicates
@@ -70,62 +89,75 @@ class AutoClean:
         self.extract_datetime = extract_datetime
         self.outlier_param = outlier_param
         self.techniques = {}
-        
+
         # validate the input parameters
         self._validate_params(output_data, verbose, logfile)
-        
+
         # initialize our class and start the autoclean process
-        self.output = self._clean_data(output_data, input_data)  
+        self.output = self._clean_data(output_data, input_data)
 
         end = timer()
 
         if not verbose:
-            print('AutoClean process completed in', round(end-start, 6), 'seconds')
+            print("AutoClean process completed in", round(end - start, 6), "seconds")
         if logfile:
-            print('Logfile saved to:', os.path.join(os.getcwd(), 'autoclean.log'))
+            print("Logfile saved to:", os.path.join(os.getcwd(), "autoclean.log"))
 
     def _validate_params(self, df, verbose, logfile):
         # function for validating the input parameters of the autolean process
-        
+
         if type(df) != pd.core.frame.DataFrame:
             raise ValueError('Invalid value for "df" parameter.')
-        if self.mode not in ['manual', 'auto']:
+        if self.mode not in ["manual", "auto"]:
             raise ValueError('Invalid value for "mode" parameter.')
-        if self.duplicates not in [False, 'auto']:
+        if self.duplicates not in [False, "auto"]:
             raise ValueError('Invalid value for "duplicates" parameter.')
-        if self.missing_num not in [False, 'auto', 'knn', 'mean', 'median', 'most_frequent', 'delete']:
+        if self.missing_num not in [
+            False,
+            "auto",
+            "knn",
+            "mean",
+            "median",
+            "most_frequent",
+            "delete",
+        ]:
             raise ValueError('Invalid value for "missing_num" parameter.')
-        if self.missing_categ not in [False, 'auto', 'knn', 'most_frequent', 'delete']:
+        if self.missing_categ not in [False, "auto", "knn", "most_frequent", "delete"]:
             raise ValueError('Invalid value for "missing_categ" parameter.')
-        if self.outliers not in [False, 'auto', 'winz', 'delete']:
+        if self.outliers not in [False, "auto", "winz", "delete"]:
             raise ValueError('Invalid value for "outliers" parameter.')
         if isinstance(self.encode_categ, list):
-            if len(self.encode_categ) > 2 and self.encode_categ[0] not in ['auto', 'onehot', 'label']:
+            if len(self.encode_categ) > 2 and self.encode_categ[0] not in [
+                "auto",
+                "onehot",
+                "label",
+            ]:
                 raise ValueError('Invalid value for "encode_categ" parameter.')
             if len(self.encode_categ) == 2:
                 if not isinstance(self.encode_categ[1], list):
                     raise ValueError('Invalid value for "encode_categ" parameter.')
         else:
-            if not self.encode_categ in ['auto', False]:
+            if not self.encode_categ in ["auto", False]:
                 raise ValueError('Invalid value for "encode_categ" parameter.')
-        if not isinstance(self.outlier_param, int) and not isinstance(self.outlier_param, float):
-            raise ValueError('Invalid value for "outlier_param" parameter.')  
-        if self.extract_datetime not in [False, 'auto', 'D','M','Y','h','m','s']:
-            raise ValueError('Invalid value for "extract_datetime" parameter.')  
+        if not isinstance(self.outlier_param, int) and not isinstance(
+            self.outlier_param, float
+        ):
+            raise ValueError('Invalid value for "outlier_param" parameter.')
+        if self.extract_datetime not in [False, "auto", "D", "M", "Y", "h", "m", "s"]:
+            raise ValueError('Invalid value for "extract_datetime" parameter.')
         if not isinstance(verbose, bool):
-            raise ValueError('Invalid value for "verbose" parameter.')  
+            raise ValueError('Invalid value for "verbose" parameter.')
         if not isinstance(logfile, bool):
-            raise ValueError('Invalid value for "logfile" parameter.')  
+            raise ValueError('Invalid value for "logfile" parameter.')
 
         return
-            
+
     def _clean_data(self, df, input_data):
         # function for starting the autoclean process
         df = df.reset_index(drop=True)
         df = Duplicates.handle(self, df)
         df = MissingValues.handle(self, df)
-        df = Outliers.handle(self, df)    
-        df = Adjust.convert_datetime(self, df) 
-        df = EncodeCateg.handle(self, df)     
+        df = Outliers.handle(self, df)
+        df = Adjust.convert_datetime(self, df)
         df = Adjust.round_values(self, df, input_data)
-        return df 
+        return df
